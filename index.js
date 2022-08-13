98551d9b4f343c921615c4e7c76114e5e0608ee5
Branson Morgan <bransox@comcast.net>
2022-08-13 09:01:25 -0400

    commit tree 29feb8f22c9153a37de8e23be7bd3905e50cd745

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData (name, email) {
+    let userEmail = {
+        name: name,
+        email: email,
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userEmail)
+    })
+    .then(res => res.json())
+    .then(obj => {
+        let h2 = document.createElement('h2');
+        h2.innerHTML = obj.id;
+        document.body.appendChild(h2);
+    })
+    .catch(err => {
+        let h3 = document.createElement('h3');
+        h3.innerHTML = err.message;
+        document.body.appendChild(h3);
+    })
+}
